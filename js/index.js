60798d6ec172c9bd643436df4c57889644d0ccb6
yamiriv1311 <riv21019@byui.edu>
2022-01-08 22:46:39 -0600

    Assignment Portal update

---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+document.querySelector("#date").textContent = document.lastModified;
+var options = {year: "numeric"};
+document.querySelector("#year").textContent = new Date().toLocaleDateString("en-US", options);
