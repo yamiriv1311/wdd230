a21c0d893aa93a0f7e665fb6c768955b78002fbb
yamiriv1311 <riv21019@byui.edu>
2022-01-30 02:56:25 -0600

    more changes

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -11,3 +11,18 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
