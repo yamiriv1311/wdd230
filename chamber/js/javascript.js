65b17b0f9f9993bb870758688910dcc1693b277f
yamiriv1311 <riv21019@byui.edu>
2022-03-26 03:07:31 -0600

    More updates on my site

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -7,7 +7,6 @@ document.querySelector("#year").textContent = new Date().toLocaleDateString("en-
 
 
 //-------------- MENU ----------------------//
-
 function toggleMenu(){
   document.getElementById("ul_navigation").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -27,8 +26,6 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-
-
 
 
 //-------------- LAZY LOADING ----------------------//
@@ -59,4 +56,13 @@ else{
   imagesToLoad.forEach(function(img) {
     loadImages(img);
   });
+}
+
+
+//-------------- MONDAY AND TUESDAYS BANNER  ----------------------//
+const date = new Date();
+const currentDate = date.getDay();
+if (currentDate == 1 || currentDate == 2) {
+	let banner = document.querySelector("#banner");
+	banner.innerHTML = `🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m`;
 }
