c2105b6ddbf9b14ba5121cd49bb03a6551c85135
yamiriv1311 <riv21019@byui.edu>
2022-04-05 03:15:37 -0500

    discover page staterd

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -68,3 +68,9 @@ if (currentDate == 1 || currentDate == 2) {
 	banner.innerHTML = `🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m`;
 }
 
+
+
+
+//------------------------------------------------ DISCOVER PAGE  ------------------------------------//
+
+
