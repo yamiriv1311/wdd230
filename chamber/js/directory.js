9a36688957cdd804fc739b357dbbf603eaabf677
yamiriv1311 <riv21019@byui.edu>
2022-04-07 04:22:36 -0500

    another json

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,6 +1,6 @@
 //--------------------------------------------- DIRECTORY PAGE  ------------------------------------//
 
-const requestURL = 'https://yamiriv1311.github.io/wdd230/chamber/data/data2.json';
+const requestURL = 'https://yamiriv1311.github.io/wdd230/chamber/data/data.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
@@ -16,24 +16,26 @@ fetch(requestURL)
 
 function displayBusiness(company) {
   // Create elements to add to the document
-  let card = document.createElement('section');
+  let card = document.createElement('block');
   let logo = document.createElement('img');
-  let name = document.createElement('h2');
-  let phone = document.createElement('p');
-  let email = document.createElement('p');
+  let name = document.createElement('h5');
+  let phone = document.createElement('h6');
+  let email = document.createElement('h7');
   let address = document.createElement('a');
   let website = document.createElement('a');
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   logo.setAttribute('src', company.imageurl);
   logo.setAttribute('alt', `Logo of ${company.name}`);
+  logo.setAttribute('class', "imageJumex");
   logo.setAttribute('loading', 'lazy');
  
 
   // Change the textContent property of the h2 and p elements to contain the companie's name, phone, and email
   name.innerHTML = `${company.name}`;
-  phone.innerHTML = `<strong>Phone: </strong>${company.phone}`;
-  email.innerHTML = `<strong>Email: </strong>${company.email}`;
+  phone.innerHTML = `<b>Phone: </b>${company.phone}`;
+  email.innerHTML = `<b>Email: </b>${company.email}`;
+  logo.innerHTML
 
   // Change the content  of the a elements to contain the companie's address and website link
   address.setAttribute('href', `${company.address}`);
@@ -45,14 +47,17 @@ function displayBusiness(company) {
   website.appendChild(aWebsite);
 
 
+  
   // Add/append the section(card) with the h2 element
-  card.appendChild(logo);
   card.appendChild(name);
+  card.appendChild(logo);
   card.appendChild(phone);
   card.appendChild(email);
-  card.appendChild(address);
-  cards.appendChild(website);
+  //card.appendChild(address);
+  //cards.appendChild(website);
 
   // Add/append the existing HTML div with the cards class with the section(card)
   cards.appendChild(card);
 }
+
+
