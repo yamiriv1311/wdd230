a5eb32abaf8c989f645333d5f87168c3efe18e25
yamiriv1311 <riv21019@byui.edu>
2022-04-11 01:40:34 -0500

    like button

--- final-project/js/javascript.js
+++ final-project/js/javascript.js
@@ -51,6 +51,42 @@ var options = {year: "numeric"};
 document.querySelector("#year").textContent = new Date().toLocaleDateString("en-US", options);
 
 
+//----------------------------------------- LIKE BUTTON ---------------------//
+
+const likesDisplay = document.querySelector(".likesNumber1");
+//const likesButton2 = document.querySelector(".likesNumber2");
+//const likesButton3 = document.querySelector(".likesNumber3");
+//const likesButton4 = document.querySelector(".likesNumber4");
+
+
+//const x = document.getElementById("menuButton");
+let btn1 = document.getElementById("likeButton1");
+//let btn2 = document.getElementById("likeButton2");
+//let btn3 = document.getElementById("likeButton3");
+//let btn4 = document.getElementById("likeButton4");
+
+
+let button1 = Number((btn1.onclick).localStorage.getItem("timesBtn1Pressed"));
+//let button2 = Number((btn2.onclick).localStorage.getItem("timesBtn2Pressed"));
+//let button3 = Number((btn3.onclick).localStorage.getItem("timesBtn3Pressed"));
+//let button4 = Number((btn4.onclick).localStorage.getItem("timesBtn4Pressed"));
+
+button1 ++;
+//button2 ++;
+//button3 ++;
+//button4 ++;
+
+ls1 = localStorage.setItem("timesBtn1Pressed", button1);
+//ls2 = localStorage.setItem("timesBtn2Pressed", button2);
+//ls3 = localStorage.setItem("timesBtn3Pressed", button3);
+//ls4 = localStorage.setItem("timesBtn4Pressed", button4);
+
+likesNumber1.innerHTML = `${ls1}`;
+//likesNumber2.innerHTML = `${ls2}`;
+//likesNumber3.innerHTML = `${ls3}`;
+//xlikesNumber4.innerHTML = `${ls4}`;
+
+
 //----------------------------------------- WEATHER---------------------//
 
 
